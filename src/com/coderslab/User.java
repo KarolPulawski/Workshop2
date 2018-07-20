0b51b1c8676c1c557ef72ce15aa5d1467217a68b
KarolPulawski <karol.j.pulawski@gmail.com>
2018-07-20 15:45:21 +0200

    Workshop 2 / BCrypt was added. User class was created.

---
+++ src/com/coderslab/User.java
@@ -0,0 +1,37 @@
+package com.coderslab;
+
+public class User {
+
+    private int id;
+    private String name;
+    private String username;
+    private String password;
+    private String email;
+
+    public User(String name, String username, String password, String email) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public User() {
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
